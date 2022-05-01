3b90dfc82ffdaebe36b0bdf2674fa24ade47385f
Jorge Castro <arturocastro.q@gmail.com>
2022-05-01 18:23:29 -0500

    FEAT Mandatory Requirements Complete

--- src/components/CreateEmployee.vue
+++ src/components/CreateEmployee.vue
@@ -1,10 +1,98 @@
 <template>    
-<div>
-    Create Employee
+<div class="container">
+    
+    <div class="card">
+        <div class="card-header">
+            <h1>NEW EMPLOYEE</h1>
+        </div>
+        <div class="card-body">
+            <form v-on:submit.prevent="insertEmployee">
+                <div class="form-group">
+                  <label for="">Age:</label>
+                  <input type="number" required
+                    class="form-control" name="age" aria-describedby="Write Employee Age" placeholder="Age" v-model="employee.age">
+                </div>
+                <div class="form-group">
+                  <label for="">Email:</label>
+                  <input type="email" required
+                    class="form-control" name="email" aria-describedby="Write Employee Email" placeholder="Email" v-model="employee.email">
+                </div>
+                <div class="form-group">
+                  <label for="">Name:</label>
+                  <input type="text" required
+                    class="form-control" name="name" aria-describedby="Write Employee Name" placeholder="Name" v-model="employee.name">
+                </div>
+                <div class="form-group">
+                  <label for="">Position:</label>
+                  <input type="text" required
+                    class="form-control" name="position" aria-describedby="Write Employee Position" placeholder="Postion" v-model="employee.position">
+                </div>
+                <div class="form-group">
+                  <label for="">Surname:</label>
+                  <input type="text" required
+                    class="form-control" name="surname" aria-describedby="Write Employee Surname" placeholder="Surname"  v-model="employee.surname">
+                </div>
+
+                <div class="btn-group" role="group" aria-label="">
+
+
+                    <button type="submit" class="btn btn-primary">AGREGATE</button>
+                    <a name="" id="" class="btn btn-danger" href="./listEmployee" role="button">CANCEL</a>
+                    
+                 
+                </div>
+            </form>
+        </div>
+     
+    </div>
 </div>
 </template>
 <script>
 export default {
     
+    data(){
+
+    return{
+     employee:{}
+
+    }
+
+    },
+    methods:{
+
+    insertEmployee: function(){
+        
+        fetch('http://jacastroq-002-site3.ftempurl.com/api/employees',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                createdBy:"TestUser"
+                ,age:this.employee.age
+                ,email:this.employee.email
+                ,name:this.employee.name
+                ,position: this.employee.position
+                ,surname:this.employee.surname
+                }
+                ),
+               headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                }
+        }
+        )
+        .then(
+            (
+                    dataNewEmployee =>{
+                    console.log(dataNewEmployee);
+                    window.location.href='listEmployee'
+                }
+            )
+        )
+
+
+    }
+
+    }
+
 }
 </script>
--- src/components/EditEmployee.vue
+++ src/components/EditEmployee.vue
@@ -1,10 +1,119 @@
 <template>    
-<div>
-    Edit Employee
+<div class="container">
+    
+    <div class="card">
+        <div class="card-header">
+            <h1>UPDATE EMPLOYEE</h1>
+        </div>
+        <div class="card-body">
+            <form v-on:submit.prevent="updateEmployee">
+                <div class="form-group">
+                  <label for="">Age:</label>
+                  <input type="number" required
+                    class="form-control" name="age" aria-describedby="Write Employee Age" placeholder="Age" v-model="employee.age">
+                </div>
+                 <div class="form-group">
+                  <label for="">Email:</label>
+                  <input type="email" required
+                    class="form-control" name="email" aria-describedby="Write Employee Email" placeholder="Email" v-model="employee.email">
+                </div>
+                <div class="form-group">
+                  <label for="">Name:</label>
+                  <input type="text" required
+                    class="form-control" name="name" aria-describedby="Write Employee Name" placeholder="Name" v-model="employee.name">
+                </div>
+                <div class="form-group">
+                  <label for="">Position:</label>
+                  <input type="text" required
+                    class="form-control" name="position" aria-describedby="Write Employee Position" placeholder="Position"  v-model="employee.position">
+                </div>
+                <div class="form-group">
+                  <label for="">Surname:</label>
+                  <input type="text" required
+                    class="form-control" name="surname" aria-describedby="Write Employee Surname" placeholder="Surname"  v-model="employee.surname">
+                </div>
+
+                <div class="btn-group" role="group" aria-label="">
+
+
+                    <button type="submit" class="btn btn-primary">UPDATE</button>
+                    <a name="" id="" class="btn btn-danger" href="../listEmployee" role="button">CANCEL</a>
+                    
+                 
+                </div>
+            </form>
+        </div>
+     
+    </div>
 </div>
 </template>
 <script>
 export default {
-    
+    data(){
+
+    return{
+     employee:{}
+    }
+
+    },    
+    created:function(){
+           this.getEmployeeID();
+    },
+    methods:{
+        getEmployeeID(){
+
+             fetch('http://jacastroq-002-site3.ftempurl.com/api/employees/'+this.$route.params.id)
+            .then(oneEmployee=>oneEmployee.json())
+            .then((dataEmployee)=>{
+             //   console.log(this.$route.params.id)
+
+                 console.log(dataEmployee)
+                 this.employee.age = dataEmployee.age;
+                 this.employee.email = dataEmployee.email;
+                 this.employee.name = dataEmployee.name;
+                 this.employee.position = dataEmployee.position;
+                 this.employee.surname = dataEmployee.surname;
+            })
+            .catch(console.log)
+
+        },
+
+        updateEmployee: function(){
+        
+        fetch('http://jacastroq-002-site3.ftempurl.com/api/employees/'+this.$route.params.id,{
+            method:"PUT",
+            body:JSON.stringify(
+                {
+                modifiedBy:"TestUser"
+                ,age:this.employee.age
+                ,email: this.employee.email
+                ,name: this.employee.name
+                ,position:this.employee.position
+                ,surname:this.employee.surname
+                }
+                ),
+               headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                }
+        }
+        )
+        .then(
+            (
+                    dataNewEmployee =>{
+                    console.log(dataNewEmployee);
+                    window.location.href='../listEmployee'
+                }
+            )
+        )
+
+
+    }
+
+
+
+    }
+
+   
 }
 </script>
